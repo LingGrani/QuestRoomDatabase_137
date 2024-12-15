65ad48159cb54e51d9d31af215e57c09c7cc7159
LingGrani <luthfiangger2510@gmail.com>
2024-12-16 02:14:20 +0700

    Add methods to MahasiswaDao

--- app/src/main/java/com/example/p9database/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/p9database/data/dao/MahasiswaDao.kt
@@ -1,8 +1,10 @@
 package com.example.p9database.data.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import com.example.p9database.data.entity.Mahasiswa
 import kotlinx.coroutines.flow.Flow
 
@@ -12,6 +14,14 @@ interface MahasiswaDao {
     fun getAllMahasiswa(): Flow<List<Mahasiswa>>
     @Insert
     suspend fun insertMahasiswa(
+        mahasiswa: Mahasiswa
+    )
+    @Delete
+    suspend fun deleteMahasiswa(
+        mahasiswa: Mahasiswa
+    )
+    @Update
+    suspend fun updateMahasiswa(
         mahasiswa: Mahasiswa
     )
 }
