beb01bda7bed9433e4da387fa56073e868b74d94
LingGrani <luthfiangger2510@gmail.com>
2024-12-16 02:04:04 +0700

    Create ViewModel factory for MahasiswaViewModel

---
+++ app/src/main/java/com/example/p9database/ui/viewmodel/PenyediaViewModel.kt
@@ -0,0 +1,19 @@
+package com.example.p9database.ui.viewmodel
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.Factory
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.p9database.KrsApp
+
+fun CreationExtras.krsApp(): KrsApp =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as KrsApp)
+
+object PenyediaViewMo del{
+    val Factory = viewModelFactory {
+        initializer {
+            MahasiswaViewModel(krsApp().containerApp.repositoryMhs)
+        }
+    }
+}
