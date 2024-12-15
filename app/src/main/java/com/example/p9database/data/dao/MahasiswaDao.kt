a3e7a7dc53afb3bc22d76892413a12e07bc5e61e
LingGrani <luthfiangger2510@gmail.com>
2024-12-16 02:12:01 +0700

    Modify @Query in getAllMahasiswa to order by nama

--- app/src/main/java/com/example/p9database/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/p9database/data/dao/MahasiswaDao.kt
@@ -8,11 +8,10 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface MahasiswaDao {
-    @Query("SELECT * FROM mahasiswa")
+    @Query("SELECT * FROM mahasiswa ORDER BY nama ASC")
     fun getAllMahasiswa(): Flow<List<Mahasiswa>>
     @Insert
     suspend fun insertMahasiswa(
         mahasiswa: Mahasiswa
     )
-
 }
