56e213c05e6e5b3e4e55d73efafd13a841af9774
LingGrani <luthfiangger2510@gmail.com>
2024-12-16 02:35:34 +0700

    Add navigation destinations with route

--- app/src/main/java/com/example/p9database/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/p9database/ui/navigation/AlamatNavigasi.kt
@@ -3,3 +3,19 @@ package com.example.p9database.ui.navigation
 interface AlamatNavigasi {
     val route: String
 }
+
+object DestinasiHome : AlamatNavigasi {
+    override val route: String = "home"
+}
+
+object DestinasiDetail: AlamatNavigasi {
+    override val route: String = "detail"
+    const val nimArg = "nim"
+    val routeWithArgs = "$route/{$nimArg}"
+}
+
+object DestinasiUpdate: AlamatNavigasi {
+    override val route: String = "update"
+    const val nimArg = "nim"
+    val routeWithArgs = "$route/{$nimArg}"
+}
